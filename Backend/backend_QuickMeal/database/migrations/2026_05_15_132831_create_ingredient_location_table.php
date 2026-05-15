d1745690b4191c33f39288473b0b9d433f99107a
shunaria <shunnoking@gmail.com>
2026-05-15 21:45:21 +0800

    model,migration,CRUD location

---
+++ Backend/backend_QuickMeal/database/migrations/2026_05_15_132831_create_ingredient_location_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * GET /api/auth/me
+     */
+    public function up(): void
+    {
+        Schema::create('ingredient_location', function (Blueprint $table) {
+
+            $table->id();
+
+            $table->unsignedBigInteger('ingredient_id');
+
+            $table->unsignedBigInteger('id_location');
+
+            $table->foreign('ingredient_id')
+                ->references('id')
+                ->on('ingredients')
+                ->onDelete('cascade');
+
+            $table->foreign('id_location')
+                ->references('id_location')
+                ->on('locations')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * GET /api/auth/me
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ingredient_location');
+    }
+};
